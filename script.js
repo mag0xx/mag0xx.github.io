91f6a04dd84f3bf9f4193c2976dc3a9fb454b120
mag0xx <149200934+mag0xx@users.noreply.github.com>
2023-10-27 13:29:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Ejemplo de interacción con JavaScript
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    alert('¡Hola, mundo!');
+});
